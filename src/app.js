39f58990f91748d24002e2802be36fdb223ccf36
cgonzalo12 <cap.gonzalo12@gmail.com>
2024-01-27 14:03:57 -0300

    agregando handlebars

--- src/app.js
+++ src/app.js
@@ -1,10 +1,42 @@
 import { productManager } from "./ProductManager.js";
-import { CartManager } from "./cartManager.js";
 import { Product } from "./Product.js";
 import express from "express";
+import handlebars from "express-handlebars";
+import __dirname from "./utils.js";
 import { productsRoutes } from "./routes/products.routes.js";
 import { cartsRouter } from "./routes/carts.routes.js";
+//inicializo express
 const app = express();
+//inicio handlebars
+app.engine("handlebars", handlebars.engine()); //motor de plantilla
+app.set("views", __dirname + "/views"); //indico donde estan las vi stas
+app.set("view engine", "handlebars"); //indica el motor de plantilla q ya iniciamos arriba
+app.use(express.static(__dirname + "/public"));
+
+// app.get("/", (req, res) => {
+//   let user = {
+//     name: "gonzalo",
+//     last_name: "capararo",
+//   };
+//   res.render("index", user);
+// });
+
+app.get("/", async (req, res) => {
+  try {
+    const limit = req.query.limit;
+    let products = await productManager.getProducts();
+    if (limit) {
+      const products = products.slice(0, limit);
+      res.render("index", products);
+    }
+    console.log(products);
+    res.render("index", { name: "gonzalo", lastName: "capararo", products });
+  } catch (error) {
+    console.log(error);
+    res.send("error !!!");
+  }
+});
+//
 
 app.use(express.json());
 app.use("/api/products", productsRoutes);
@@ -12,7 +44,7 @@ app.use("/api/carts", cartsRouter);
 
 const port = 8080;
 
-app.listen(port, (req, res) => {
+const server = app.listen(port, (req, res) => {
   console.log(`escuchando el puerto ${port}`);
 });
 
@@ -44,9 +76,9 @@ const product3 = new Product(
   25
 );
 
-const init = async () => {
-  await productManager.addProduct(product1);
-  await productManager.addProduct(product2);
-  await productManager.addProduct(product3);
-};
-init();
+// const init = async () => {
+//   await productManager.addProduct(product1);
+//   await productManager.addProduct(product2);
+//   await productManager.addProduct(product3);
+// };
+// init();
