8f01ca1d4cac83c2d1c8ab4c3942340cb568ab64
cgonzalo12 <cap.gonzalo12@gmail.com>
2024-02-12 20:56:52 -0300

    1er commit

--- src/app.js
+++ src/app.js
@@ -1,17 +1,17 @@
-import { productManager } from "./ProductManager.js";
-import { Product } from "./Product.js";
 import express from "express";
 import handlebars from "express-handlebars";
-import __dirname from "./utils.js";
+import __dirname from "./utils/utils.js";
 import { productsRoutes } from "./routes/products.routes.js";
 import { cartsRouter } from "./routes/carts.routes.js";
 import { Server } from "socket.io";
 import viewsRouter from "./routes/views.routes.js";
+import { productManager } from "./ProductManager.js";
 const port = 8080;
 //inicializo express
 const app = express();
 
 //socket
+const mensajes = [];
 
 const httpServer = app.listen(port, (req, res) => {
   console.log(`escuchando el puerto ${port}`);
@@ -19,58 +19,55 @@ const httpServer = app.listen(port, (req, res) => {
 const socketServer = new Server(httpServer);
 socketServer.on("connection", (socket) => {
   console.log("nuevo cliente conectado!");
+
+  socket.on("message", (data) => {
+    mensajes.push({ socketId: socket.id, mensaje: data });
+    socketServer.emit("message", data);
+    socketServer.emit("messages", mensajes);
+  });
+  socket.emit("messages", mensajes);
+
+  //lista de productos
+  socketServer.emit("listaProd", async () => {
+    try {
+      let products = await productManager.getProducts();
+      socketServer.emit("lista", products);
+    } catch (error) {
+      console.log(`error al cargar productos, ${{ error }} `);
+    }
+  });
+
+  //eventop para el acutal
+  socket.emit(
+    "evento_para_socket_individual",
+    "este mensaje solo lo debe recibir el socket actual!"
+  );
+  //evento para los demas, menos para el actual
+  socket.broadcast.emit(
+    "evento_para_todos_menos_actual",
+    "se conecto otro cliente!"
+  );
+  //evento para todos
+  socketServer.emit(
+    "evento_para_todos",
+    "este evento esta sicneod escuchado por todos!!!"
+  );
 });
-//inicio handlebars
+//view engine
 app.engine("handlebars", handlebars.engine()); //motor de plantilla
 app.set("views", __dirname + "/views"); //indico donde estan las vi stas
 app.set("view engine", "handlebars"); //indica el motor de plantilla q ya iniciamos arriba
+
+//public folder
 app.use(express.static(__dirname + "/public"));
 
+//milwares request
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//router products
 app.use("/products", productsRoutes);
+//routes catrs
 app.use("/api/carts", cartsRouter);
-app.use("/socket", viewsRouter);
-
-//productos
-const product1 = new Product(
-  "producto prueba",
-  "este es un producto prueba",
-  1500,
-  "sin imagen",
-  "abc123",
-  10
-);
-
-const product2 = new Product(
-  "producto prueba2",
-  "este es un producto prueba",
-  22000,
-  "sin imagen",
-  "abc1234",
-  2
-);
-
-const product3 = new Product(
-  "producto prueba3",
-  "este es un producto prueba",
-  100,
-  "sin imagen",
-  "abc12347",
-  25
-);
-const product4 = new Product(
-  "producto prueba4",
-  "producto prueba 4",
-  10,
-  "sin imagen",
-  "abc1234722",
-  5
-);
-
-// const init = async () => {
-//   await productManager.addProduct(product1);
-//   await productManager.addProduct(product2);
-//   await productManager.addProduct(product3);
-//   await productManager.addProduct(product4);
-// };
-// init();
+//routes realtime
+app.use("/realtimeproducts", viewsRouter);
--- src/public/js/socket.js
+++ src/public/js/socket.js
@@ -1,1 +1,61 @@
 const socket = io();
+// socket.emit("message", "hola me estoy comunicando desde un websocket");
+
+//escucho evento para socket actual y no los demas
+socket.on("evento_para_socket_individual", (data) => {
+  console.log(data);
+});
+
+//escucho evento para los demas socket pero no para el actual
+socket.on("evento_para_todos_menos_actual", (data) => {
+  console.log(data);
+});
+
+//evento para todos
+socket.on("evento_para_todos", (data) => {
+  console.log(data);
+});
+
+const productContainer = document.getElementById("productContainer");
+//lista de productos
+socket.on("lista", (data) => {
+  const productoHTML = data
+    .map((element) => {
+      return `<h3>Producto: </h3>
+      <p> Nombre: ${element.title} </p>
+      <p> Descripcion: ${element.description} </p>
+      <p> Stick: ${element.stock} </p>
+      <p> Precio: ${element.price} </p>
+      <p> Codigo: ${element.code} </p>
+      `;
+    })
+    .join("");
+  productContainer.innerHTML = productoHTML;
+});
+
+const messageInput = document.getElementById("messageInput");
+const sendMessageButton = document.getElementById("sendMessageButton");
+const messageContainer = document.getElementById("messageContainer");
+
+//productos
+
+sendMessageButton.addEventListener("click", () => {
+  const inputText = messageInput.value;
+  socket.emit("message", inputText);
+  messageInput.value = "";
+});
+
+socket.on("messages", (mensajes) => {
+  const mensajesHTML = mensajes
+    .map((mensaje) => {
+      console.log(mensaje.mensaje);
+      return `<p> ID: ${mensaje.socketId} : ${mensaje.mensaje} </p>`;
+    })
+    .join("");
+  messageContainer.innerHTML = mensajesHTML;
+});
+
+socket.on("products", (product) => {
+  console.log("entre");
+  console.log(product);
+});
